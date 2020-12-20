fc3ecdcacc56bbcf94f56c251ccd84f0d5c76cd4
l0r5 <lars-braitinger@web.de>
2020-12-20 04:17:59 +0100

    Fix failing test

--- src/test/java/ch/l0r5/autotrader/AutoTraderApplicationTests.java
+++ src/test/java/ch/l0r5/autotrader/AutoTraderApplicationTests.java
@@ -2,8 +2,10 @@ package ch.l0r5.autotrader;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AutoTraderApplicationTests {
 
 	@Test
